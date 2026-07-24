0ce9d9290255da01f38c0c29eef85ac2acabd677
alan-chala <alan.chala000@gmail.com>
2026-07-24 14:49:25 -0500

    feat(simple-homepage): implement dark mode theme toggle functionality

--- simple-homepage/assets/js/app.js
+++ simple-homepage/assets/js/app.js
@@ -3,24 +3,24 @@ const logoImg = document.querySelector("img");
 const moonIcon = document.querySelector(".moon");
 const sunIcon = document.querySelector(".sun");
 
-const BODY = document.body;
+const THEME_ROOT = document.documentElement;
 
 const activeTheme = localStorage.getItem("active-theme");
 
 if (activeTheme == "dark") {
-  BODY.classList.add("dark-mode");
+  THEME_ROOT.classList.add("dark-mode");
   logoImg.src = "./assets/images/logo-dark.svg";
   moonIcon.src = "./assets/images/Moon_fill.svg";
 } else {
-  BODY.classList.remove("dark-mode");
+  THEME_ROOT.classList.remove("dark-mode");
   logoImg.src = "./assets/images/logo-light.svg";
   moonIcon.src = "./assets/images/Moon_fill_light.svg";
 }
 
 toggleBtn.addEventListener("click", () => {
-  BODY.classList.toggle("dark-mode");
+  THEME_ROOT.classList.toggle("dark-mode");
 
-  if (BODY.classList.contains("dark-mode")) {
+  if (THEME_ROOT.classList.contains("dark-mode")) {
     logoImg.src = "./assets/images/logo-dark.svg";
     moonIcon.src = "./assets/images/Moon_fill.svg";
     localStorage.setItem("active-theme", "dark");
